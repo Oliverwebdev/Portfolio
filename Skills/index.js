28c4a961c6725390ad9df4cbf5510a342e5eee4f
dci-student <oliver.sporl@dci-student.org>
2024-04-02 11:46:26 +0200

    skills edited

--- Skills/index.js
+++ Skills/index.js
@@ -3,22 +3,26 @@ fetch("skills.json")
   .then((data) => {
     const skillsList = document.getElementById("skills-list");
 
-    data.forEach((skill) => {
-      const listItem = document.createElement("li");
-      const icon = document.createElement("i");
+    // Iteriere über das Array in Schritten von 2, da jede Kategorie mit einem Titel gefolgt von einem Array von Skills kommt
+    for (let i = 1; i < data.length; i += 2) {
+      data[i].forEach((skill) => {
+        const listItem = document.createElement("li");
+        const icon = document.createElement("i");
 
-      // Füge die Klassen hinzu
-      icon.className = `icon ${skill.icon}`;
-      icon.style.color = skill.color; // Setze die Farbe basierend auf den Daten aus der JSON-Datei
+        // Füge die Klassen hinzu
+        icon.className = `icon ${skill.icon}`;
+        icon.style.color = skill.color; // Setze die Farbe basierend auf den Daten aus der JSON-Datei
 
-      const skillName = document.createTextNode(skill.name);
+        const skillName = document.createTextNode(skill.name);
 
-      listItem.appendChild(icon);
-      listItem.appendChild(skillName);
-      skillsList.appendChild(listItem);
-    });
+        listItem.appendChild(icon);
+        listItem.appendChild(skillName);
+        skillsList.appendChild(listItem);
+      });
+    }
   })
   .catch((error) => console.error("Error fetching skills:", error));
+
 
 // ANFANG CODE
 // // Lade die Skills aus der JSON-Datei und erzeuge zufällig fallende Icons
